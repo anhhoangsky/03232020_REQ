266c7c0531dd6f04096dd051a365d68ccfeb8bcf
anhsky <an38570@gmail.com>
2020-03-24 20:14:31 +0700

    Add project files.

---
+++ 03232020_REQ/FormSinhVien.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace _03232020_REQ
+{
+    partial class FormSinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.controlpn = new System.Windows.Forms.Panel();
+            this.Xoasinhvienbtn = new System.Windows.Forms.Button();
+            this.Suasinhvienbtn = new System.Windows.Forms.Button();
+            this.Themsinhvienbtn = new System.Windows.Forms.Button();
+            this.SinhvienPhonetb = new System.Windows.Forms.TextBox();
+            this.SinhvienEmailtb = new System.Windows.Forms.TextBox();
+            this.SinhvienAddrtb = new System.Windows.Forms.TextBox();
+            this.SinhvienNametb = new System.Windows.Forms.TextBox();
+            this.SinhvienIDtb = new System.Windows.Forms.TextBox();
+            this.SinhvienPrkIDtb = new System.Windows.Forms.TextBox();
+            this.SinhvienPhonelb = new System.Windows.Forms.Label();
+            this.SinhvienEmaillb = new System.Windows.Forms.Label();
+            this.SinhvienAddrlb = new System.Windows.Forms.Label();
+            this.SinhVienNamelb = new System.Windows.Forms.Label();
+            this.SinhvienIDlb = new System.Windows.Forms.Label();
+            this.SinhvienPrklb = new System.Windows.Forms.Label();
+            this.viewpn = new System.Windows.Forms.Panel();
+            this.controlpn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // controlpn
+            // 
+            this.controlpn.Controls.Add(this.Xoasinhvienbtn);
+            this.controlpn.Controls.Add(this.Suasinhvienbtn);
+            this.controlpn.Controls.Add(this.Themsinhvienbtn);
+            this.controlpn.Controls.Add(this.SinhvienPhonetb);
+            this.controlpn.Controls.Add(this.SinhvienEmailtb);
+            this.controlpn.Controls.Add(this.SinhvienAddrtb);
+            this.controlpn.Controls.Add(this.SinhvienNametb);
+            this.controlpn.Controls.Add(this.SinhvienIDtb);
+            this.controlpn.Controls.Add(this.SinhvienPrkIDtb);
+            this.controlpn.Controls.Add(this.SinhvienPhonelb);
+            this.controlpn.Controls.Add(this.SinhvienEmaillb);
+            this.controlpn.Controls.Add(this.SinhvienAddrlb);
+            this.controlpn.Controls.Add(this.SinhVienNamelb);
+            this.controlpn.Controls.Add(this.SinhvienIDlb);
+            this.controlpn.Controls.Add(this.SinhvienPrklb);
+            this.controlpn.Location = new System.Drawing.Point(0, 2);
+            this.controlpn.Name = "controlpn";
+            this.controlpn.Size = new System.Drawing.Size(1267, 197);
+            this.controlpn.TabIndex = 0;
+            // 
+            // Xoasinhvienbtn
+            // 
+            this.Xoasinhvienbtn.Location = new System.Drawing.Point(1000, 121);
+            this.Xoasinhvienbtn.Name = "Xoasinhvienbtn";
+            this.Xoasinhvienbtn.Size = new System.Drawing.Size(226, 42);
+            this.Xoasinhvienbtn.TabIndex = 14;
+            this.Xoasinhvienbtn.Text = "Xoá";
+            this.Xoasinhvienbtn.UseVisualStyleBackColor = true;
+            // 
+            // Suasinhvienbtn
+            // 
+            this.Suasinhvienbtn.Location = new System.Drawing.Point(1000, 64);
+            this.Suasinhvienbtn.Name = "Suasinhvienbtn";
+            this.Suasinhvienbtn.Size = new System.Drawing.Size(226, 42);
+            this.Suasinhvienbtn.TabIndex = 13;
+            this.Suasinhvienbtn.Text = "Sửa";
+            this.Suasinhvienbtn.UseVisualStyleBackColor = true;
+            // 
+            // Themsinhvienbtn
+            // 
+            this.Themsinhvienbtn.Location = new System.Drawing.Point(1000, 9);
+            this.Themsinhvienbtn.Name = "Themsinhvienbtn";
+            this.Themsinhvienbtn.Size = new System.Drawing.Size(226, 42);
+            this.Themsinhvienbtn.TabIndex = 12;
+            this.Themsinhvienbtn.Text = "Thêm";
+            this.Themsinhvienbtn.UseVisualStyleBackColor = true;
+            // 
+            // SinhvienPhonetb
+            // 
+            this.SinhvienPhonetb.Location = new System.Drawing.Point(648, 62);
+            this.SinhvienPhonetb.Name = "SinhvienPhonetb";
+            this.SinhvienPhonetb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienPhonetb.TabIndex = 11;
+            // 
+            // SinhvienEmailtb
+            // 
+            this.SinhvienEmailtb.Location = new System.Drawing.Point(648, 19);
+            this.SinhvienEmailtb.Name = "SinhvienEmailtb";
+            this.SinhvienEmailtb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienEmailtb.TabIndex = 10;
+            // 
+            // SinhvienAddrtb
+            // 
+            this.SinhvienAddrtb.Location = new System.Drawing.Point(170, 143);
+            this.SinhvienAddrtb.Name = "SinhvienAddrtb";
+            this.SinhvienAddrtb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienAddrtb.TabIndex = 9;
+            // 
+            // SinhvienNametb
+            // 
+            this.SinhvienNametb.Location = new System.Drawing.Point(170, 105);
+            this.SinhvienNametb.Name = "SinhvienNametb";
+            this.SinhvienNametb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienNametb.TabIndex = 8;
+            // 
+            // SinhvienIDtb
+            // 
+            this.SinhvienIDtb.Location = new System.Drawing.Point(170, 62);
+            this.SinhvienIDtb.Name = "SinhvienIDtb";
+            this.SinhvienIDtb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienIDtb.TabIndex = 7;
+            // 
+            // SinhvienPrkIDtb
+            // 
+            this.SinhvienPrkIDtb.Location = new System.Drawing.Point(170, 21);
+            this.SinhvienPrkIDtb.Name = "SinhvienPrkIDtb";
+            this.SinhvienPrkIDtb.Size = new System.Drawing.Size(282, 22);
+            this.SinhvienPrkIDtb.TabIndex = 6;
+            // 
+            // SinhvienPhonelb
+            // 
+            this.SinhvienPhonelb.AutoSize = true;
+            this.SinhvienPhonelb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhvienPhonelb.Location = new System.Drawing.Point(489, 64);
+            this.SinhvienPhonelb.Name = "SinhvienPhonelb";
+            this.SinhvienPhonelb.Size = new System.Drawing.Size(119, 20);
+            this.SinhvienPhonelb.TabIndex = 5;
+            this.SinhvienPhonelb.Text = "SinhvienPhone";
+            // 
+            // SinhvienEmaillb
+            // 
+            this.SinhvienEmaillb.AutoSize = true;
+            this.SinhvienEmaillb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhvienEmaillb.Location = new System.Drawing.Point(489, 21);
+            this.SinhvienEmaillb.Name = "SinhvienEmaillb";
+            this.SinhvienEmaillb.Size = new System.Drawing.Size(114, 20);
+            this.SinhvienEmaillb.TabIndex = 4;
+            this.SinhvienEmaillb.Text = "SinhvienEmail";
+            // 
+            // SinhvienAddrlb
+            // 
+            this.SinhvienAddrlb.AutoSize = true;
+            this.SinhvienAddrlb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhvienAddrlb.Location = new System.Drawing.Point(28, 143);
+            this.SinhvienAddrlb.Name = "SinhvienAddrlb";
+            this.SinhvienAddrlb.Size = new System.Drawing.Size(107, 20);
+            this.SinhvienAddrlb.TabIndex = 3;
+            this.SinhvienAddrlb.Text = "SinhvienAddr";
+            // 
+            // SinhVienNamelb
+            // 
+            this.SinhVienNamelb.AutoSize = true;
+            this.SinhVienNamelb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhVienNamelb.Location = new System.Drawing.Point(28, 105);
+            this.SinhVienNamelb.Name = "SinhVienNamelb";
+            this.SinhVienNamelb.Size = new System.Drawing.Size(119, 20);
+            this.SinhVienNamelb.TabIndex = 2;
+            this.SinhVienNamelb.Text = "SinhVienName";
+            // 
+            // SinhvienIDlb
+            // 
+            this.SinhvienIDlb.AutoSize = true;
+            this.SinhvienIDlb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhvienIDlb.Location = new System.Drawing.Point(28, 64);
+            this.SinhvienIDlb.Name = "SinhvienIDlb";
+            this.SinhvienIDlb.Size = new System.Drawing.Size(89, 20);
+            this.SinhvienIDlb.TabIndex = 1;
+            this.SinhvienIDlb.Text = "SinhvienID";
+            // 
+            // SinhvienPrklb
+            // 
+            this.SinhvienPrklb.AutoSize = true;
+            this.SinhvienPrklb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SinhvienPrklb.Location = new System.Drawing.Point(29, 21);
+            this.SinhvienPrklb.Name = "SinhvienPrklb";
+            this.SinhvienPrklb.Size = new System.Drawing.Size(114, 20);
+            this.SinhvienPrklb.TabIndex = 0;
+            this.SinhvienPrklb.Text = "SinhvienPrkID";
+            // 
+            // viewpn
+            // 
+            this.viewpn.Location = new System.Drawing.Point(-1, 215);
+            this.viewpn.Name = "viewpn";
+            this.viewpn.Size = new System.Drawing.Size(1267, 308);
+            this.viewpn.TabIndex = 1;
+            // 
+            // FormSinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 522);
+            this.Controls.Add(this.viewpn);
+            this.Controls.Add(this.controlpn);
+            this.Name = "FormSinhVien";
+            this.Text = "Quản Lý Sinh Viên";
+            this.controlpn.ResumeLayout(false);
+            this.controlpn.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel controlpn;
+        private System.Windows.Forms.Label SinhvienPhonelb;
+        private System.Windows.Forms.Label SinhvienEmaillb;
+        private System.Windows.Forms.Label SinhvienAddrlb;
+        private System.Windows.Forms.Label SinhVienNamelb;
+        private System.Windows.Forms.Label SinhvienIDlb;
+        private System.Windows.Forms.Label SinhvienPrklb;
+        private System.Windows.Forms.Panel viewpn;
+        private System.Windows.Forms.Button Xoasinhvienbtn;
+        private System.Windows.Forms.Button Suasinhvienbtn;
+        private System.Windows.Forms.Button Themsinhvienbtn;
+        private System.Windows.Forms.TextBox SinhvienPhonetb;
+        private System.Windows.Forms.TextBox SinhvienEmailtb;
+        private System.Windows.Forms.TextBox SinhvienAddrtb;
+        private System.Windows.Forms.TextBox SinhvienNametb;
+        private System.Windows.Forms.TextBox SinhvienIDtb;
+        private System.Windows.Forms.TextBox SinhvienPrkIDtb;
+    }
+}
+
---
+++ 03232020_REQ/SinhvienDBMng.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03232020_REQ
+{
+    class SinhvienDBMng
+    {
+
+    }
+}
